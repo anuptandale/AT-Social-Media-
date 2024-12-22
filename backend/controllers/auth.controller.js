39b4c316121b287fc532782f5c583ba4c11c4fa5
anup <anupdineshtandale@gmail.com>
2024-12-22 11:37:00 +0530

    1-project setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,17 @@
+export const signup = async (req, res)=>{
+    res.json({
+        data: "You hit the signup endpoint",
+    })
+}
+
+export const signin = async (req, res)=>{
+    res.json({
+        data: "You hit the signin endpoint",
+    })
+}
+
+export const logout = async (req, res)=>{
+    res.json({
+        data: "You hit the logout endpoint",
+    })
+}
